f0dfe040fde5285d5b19cd413cdb990c35638652
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-12 09:28:04 +0100

    setup typescript

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,28 @@
+interface Director {
+  firstName: string;
+  lastName: string;
+  workDirectorTasks(): string;
+}
+
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  workTeacherTasks(): string;
+}
+
+type Employee = Director | Teacher;
+
+// ✅ Function: Type Predicate
+function isDirector(employee: Employee): employee is Director {
+  // You can check by the presence of `workDirectorTasks`
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// ✅ Function: Execute Work
+function executeWork(employee: Employee): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
